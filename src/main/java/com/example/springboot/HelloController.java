b515d09f894b9ffaeb86b6c85f4d9fcd392310d1
Quoc Huy Pham <phamquochuy2905@gmail.com>
2021-05-09 13:01:02 +0200

    Master: Text changes

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,18 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello World!";
+        return "Hello World";
+    }
+
+    @GetMapping("/")
+        String hello() {
+        return "Deployed!";
     }
 
 }
